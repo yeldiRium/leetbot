1997a7f9e14e4f1a707917239eef88b5c3cc0290
Hannes Leutloff <hannes.leutloff@aol.de>
2018-11-08 22:28:49 +0100

    leetbot: update main logic

--- leetbot/index.js
+++ leetbot/index.js
@@ -1,44 +1,89 @@
+import { writeFileSync, readFileSync, existsSync } from 'fs'
 import Telegraf from 'telegraf'
+import * as R from 'ramda'
+import { createStore } from 'redux'
+import rootReducer from './reducer'
+import { enableChat, disableChat } from './actions'
+import { chatIdInContext, messageInContext } from '../util/telegram'
 
-import { isCurrentlyLeet, leftPad } from './util'
-import { rootRedux, counterUpdate } from './redux'
-import { sendReminderIfLeet } from './timeout'
+import i18n from './i18n'
 
-let state
+/**
+ * Load a startup state from a dump file, if it exists.
+ * Otherwise return undefined.
+ *
+ * @param String fileName
+ * @return {*} state
+ */
+const loadState = (dumpFile) => {
+  if (existsSync(dumpFile)) {
+    console.info(`loading state from ${dumpFile}`)
+    return JSON.parse(readFileSync(dumpFile))
+  }
+  console.info(`${dumpFile} doesn't exist; starting with empty state`)
+  return undefined
+}
 
-export default (token, { chatId, leetHours, leetMinutes }, telegramOptions) => {
+/**
+ * Dump a given state object into a dump file.
+ * @param String dumpFile
+ * @param {*} state
+ */
+const dumpState = (dumpFile, state) => writeFileSync(
+  dumpFile, JSON.stringify(state), { flag: 'w+' }
+)
+
+/**
+ * Catch and log any error that might occur further down the line.
+ *
+ * @param {*} ctx
+ * @param {*} next
+ */
+const crashHandler = (ctx, next) => {
+  return next().catch(console.error)
+}
+
+export default (token, { chatId, leetHours, leetMinutes, dumpFile }, telegramOptions) => {
+  console.log('leetbot starting...')
   const bot = new Telegraf(token, telegramOptions)
+  const store = createStore(rootReducer, loadState())
 
-  // check every minute if reminder needs to be sent
-  setInterval(() => {
-    sendReminderIfLeet(chatId, bot.telegram)
-  }, 60000)
+  setInterval(
+    () => {
+      console.log('dumping state')
+      dumpState(dumpFile, store.getState())
+    },
+    1000
+  )
+
+  bot.use(crashHandler)
 
   bot.start(ctx => {
-    ctx.reply('Hallo i bims, 1 LeetBot. I zaehl euere Leetposts vong Heaufigkiet hern.')
+    ctx.reply(i18n.t('start'))
   })
 
-  bot.command('debug', ctx => {
-    // Fucking timezones.
-    let debug = `Leet-Time is ${leftPad(leetHours + 1)(2, '0')}:${leftPad(leetMinutes)(2, '0')}.\n`
-    if (state === undefined) {
-      debug += 'State not initialized.'
-    } else {
-      debug += JSON.stringify(state, null, 2)
-    }
-    ctx.reply(debug)
+  bot.command('enable', ctx => {
+    store.dispatch(enableChat(chatIdInContext(ctx)))
+    ctx.reply(i18n.t('enable chat'))
   })
 
-  bot.hears(/.*/, ctx => {
-    if (isCurrentlyLeet()) {
-      state = rootRedux(counterUpdate(ctx), state)
-    }
+  bot.command('disable', ctx => {
+    store.dispatch(disableChat(chatIdInContext(ctx)))
+    ctx.reply(i18n.t('disable chat'))
   })
+
+  bot.command('setLanguage', ctx =>
+    i18n.changeLanguage(
+      messageInContext(ctx).split(' ').slice(-1)[0],
+      (err, t) => {
+        if (err) {
+          ctx.reply(i18n.t('language unknown'))
+          return
+        }
+        ctx.reply(i18n.t('language changed'))
+      }
+    )
+  )
 
   bot.startPolling()
-
-  return {
-    shutDown () {
-    }
-  }
 }
