e5e23d2de262ad3e42659b1b3810435fa80ae531
Hannes Leutloff <hannes.leutloff@aol.de>
2018-11-08 17:57:15 +0100

    add changes for util functions

--- leetbot/__tests__/util.test.js
+++ leetbot/__tests__/util.test.js
@@ -1,4 +1,11 @@
-import { isTimeForReminder, isCurrentlyLeet } from '../util'
+import {
+  isTimeForReminder,
+  isCurrentlyLeet,
+  userInContext,
+  messageInContext,
+  isLeetLegit,
+  leftPad
+} from '../util'
 
 const mockLeetbotConfig = config => ({
   loadConfig: () => ({
@@ -24,7 +31,7 @@ describe('util', () => {
   describe('isTimeForReminder', () => {
     afterEach(resetDate)
 
-    it('should return true, if it is one minute before leet', () => {
+    it('should be true, if it is one minute before leet', () => {
       mockLeetbotConfig({
         config: {
           leetHours: 13,
@@ -40,7 +47,7 @@ describe('util', () => {
       expect(isTimeForReminder()).toBe(true)
     })
 
-    it('should return false, if it is not one minute before leet', () => {
+    it('should be false, if it is not one minute before leet', () => {
       mockLeetbotConfig({
         config: {
           leetHours: 13,
@@ -60,7 +67,7 @@ describe('util', () => {
   describe('isCurrentlyLeet', () => {
     afterEach(resetDate)
 
-    it('should return true, if it is currently leet', () => {
+    it('should be true, if it is currently leet', () => {
       mockLeetbotConfig({
         config: {
           leetHours: 13,
@@ -76,7 +83,7 @@ describe('util', () => {
       expect(isCurrentlyLeet()).toBe(true)
     })
 
-    it('should return false, if it is not currently leet', () => {
+    it('should be false, if it is not currently leet', () => {
       mockLeetbotConfig({
         config: {
           leetHours: 13,
@@ -90,6 +97,97 @@ describe('util', () => {
       mockDate(testTime.toISOString())
 
       expect(isCurrentlyLeet()).toBe(false)
+    })
+  })
+
+  describe('userInContext', () => {
+    it('should get the responsible user from a telegraf context', () => {
+      const mockUser = {
+        some: 'data',
+        doesnt: 'matter'
+      }
+      const mockContext = {
+        update: {
+          message: {
+            from: mockUser
+          }
+        }
+      }
+
+      expect(userInContext(mockContext)).toEqual(mockUser)
+    })
+
+    it('should be undefined on a malformed context', () => {
+      const mockContext = {
+        lel: 'this',
+        context: 'is broken'
+      }
+
+      expect(userInContext(mockContext)).toBeUndefined()
+    })
+  })
+
+  describe('messageInContext', () => {
+    it('should get the message text from a telegraf context', () => {
+      const mockText = 'this is the message text that should be returned'
+      const mockContext = {
+        update: {
+          message: {
+            text: mockText
+          }
+        }
+      }
+
+      expect(messageInContext(mockContext)).toEqual(mockText)
+    })
+
+    it('should be undefined on a malformed context', () => {
+      const mockContext = {
+        lel: 'this',
+        context: 'is broken'
+      }
+
+      expect(messageInContext(mockContext)).toBeUndefined()
+    })
+  })
+
+  describe('isLeetLegit', () => {
+    it('should be true if the user is not yet leet and the message is exactly "1337"', () => {
+      expect(isLeetLegit([], '1337', 'newUser')).toBe(true)
+    })
+    it('should be false if the user is already leet', () => {
+      expect(isLeetLegit(['leetUser'], '1337', 'leetUser')).toBe(false)
+    })
+    it('should be false if the message is not exactly 1337', () => {
+      const wrongLeets = [
+        ' 1337',
+        '137',
+        '13 37',
+        '13:37',
+        'etc',
+        'you get the gist'
+      ]
+
+      wrongLeets.forEach(
+        wrongLeet => expect(isLeetLegit([], wrongLeet, 'user')).toBe(false)
+      )
+    })
+  })
+
+  describe('leftPad', () => {
+    it('should dispatch to the builtin String.padStart method after converting the input to String', () => {
+      const testCases = [
+        ['someString', 12, '0', '00someString'],
+        [13, 5, ' ', '   13'],
+        [12345, 2, 'a', '12345'],
+        ['ab', 6, 'ab', 'ababab']
+      ]
+      testCases.forEach(
+        testCase => {
+          const [inputString, length, padChar, expected] = testCase
+          expect(leftPad(inputString)(length, padChar)).toEqual(expected)
+        }
+      )
     })
   })
 })
