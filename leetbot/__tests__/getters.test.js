555ae8bb1bf0eacc87ba952170423d57128cbd24
Hannes Leutloff <hannes.leutloff@aol.de>
2019-01-04 08:34:55 +0100

    [TASK] add tests for the getter `isLeetInChatAborted`

--- leetbot/__tests__/getters.test.js
+++ leetbot/__tests__/getters.test.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux'
 import app from '../reducer'
-import { enableChat, updateRecord } from '../actions'
-import { recordInChat } from '../getters'
+import { enableChat, updateRecord, abortLeet, restartLeet } from '../actions'
+import { recordInChat, isLeetInChatAborted } from '../getters'
 
 describe('recordInChat', () => {
   it('throws an error for disabled chats', () => {
@@ -34,5 +34,25 @@ describe('recordInChat', () => {
     })
 
     expect(recordInChat(chatId, store)).toEqual(0)
+  })
+})
+
+describe('isLeetInChatAborted', () => {
+  it('returns false in the initial state', () => {
+    const store = createStore(app)
+    const chatId = 'someChatId'
+
+    store.dispatch(restartLeet(chatId))
+    expect(isLeetInChatAborted(chatId, store)).toBeFalse()
+  })
+
+  it('returns true, if leet was aborted beforehand', () => {
+    const store = createStore(app)
+    const chatId = 'someChatId'
+    const asshole = 'someAsshole'
+
+    store.dispatch(enableChat(chatId))
+    store.dispatch(abortLeet(asshole, chatId))
+    expect(isLeetInChatAborted(chatId, store)).toBeTrue()
   })
 })
