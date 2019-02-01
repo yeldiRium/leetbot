41cb8d24c756df82735e1ecaec01b48d824541a2
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2019-02-01 17:02:28 +0100

    [BUGFIX] fix re-/unpinning after leet

--- leetbot/index.js
+++ leetbot/index.js
@@ -73,7 +73,9 @@ const scheduleJobs = ({
     const chats = await reminder(bot, store, i18n)
     scheduler.scheduleJob(
       moment().seconds(0).minutes(leetMinutes + 1).toDate(),
-      () => reOrUnpin(bot, chats, i18n)
+      () => {
+        reOrUnpin(bot, chats, i18n)
+      }
     )
   })
   scheduler.scheduleJob(`57 ${leetMinutes - 1} ${leetHours} * * *`, () => {
