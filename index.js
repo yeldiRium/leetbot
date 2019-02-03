69e9b62cf14bf0ddf81af725c4f1a2f5a09c4d10
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2019-02-03 15:46:41 +0100

    [FEATURE] enable sentry

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+import Sentry from '@sentry/node'
+
 import { loadConfig } from './config'
 import { validToken } from './util'
 
 import leetbot from './leetbot'
+
+Sentry.init({
+  dsn: 'https://4f836faef7194e01aab78b7c1dcf37d8:dd43773e5b10458ead71162febb9cad9@sentry.marvelous.systems/3'
+})
 
 const config = loadConfig()
 
