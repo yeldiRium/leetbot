abd5ffabb82cb0f6226514f7e3e45fdda7fcd8b4
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2019-02-08 21:19:05 +0100

    [TASK] write tests and fix some bugs in config module
    This resolves LEET-5

--- util/config.js
+++ util/config.js
@@ -16,7 +16,7 @@ const loadConfig = () => {
       token: R.defaultTo('', process.env.LEETBOT_TOKEN),
       username: R.defaultTo('', process.env.LEETBOT_USERNAME),
       config: {
-        commit: process.env.COMMIT,
+        commit: R.defaultTo('', process.env.COMMIT),
         leetHours: parseHours(R.defaultTo(13, process.env.LEETBOT_HOURS), timezone),
         leetMinutes: parseMinutes(R.defaultTo(37, process.env.LEETBOT_MINUTES), timezone),
         dumpFile: String(R.defaultTo('./leetbot/dump.json', process.env.LEETBOT_DUMP_FILE)),
