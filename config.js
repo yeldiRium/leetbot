ecaf9f3bcafeccd5620dc422080355a7bdad39db
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2019-02-08 20:35:55 +0100

    [TASK] move timezone selection into `loadConfig()`

--- config.js
+++ config.js
@@ -2,29 +2,31 @@ import * as R from 'ramda'
 
 import { parseHours, parseMinutes } from './util/time'
 
-const timezone = R.defaultTo('Europe/Berlin', process.env.TIMEZONE)
-console.log(`timezone: ${timezone}`)
+const loadConfig = () => {
+  const timezone = R.defaultTo('Europe/Berlin', process.env.TIMEZONE)
+  console.log(`timezone: ${timezone}`)
 
-const loadConfig = () => ({
-  examplebot: {
-    token: R.defaultTo('', process.env.EXAMPLEBOT_TOKEN),
-    username: R.defaultTo('', process.env.EXAMPLEBOT_USERNAME),
-    config: {}
-  },
-  leetbot: {
-    token: R.defaultTo('', process.env.LEETBOT_TOKEN),
-    username: R.defaultTo('', process.env.LEETBOT_USERNAME),
-    config: {
-      commit: process.env.COMMIT,
-      leetHours: parseHours(R.defaultTo(13, process.env.LEETBOT_HOURS), timezone),
-      leetMinutes: parseMinutes(R.defaultTo(37, process.env.LEETBOT_MINUTES), timezone),
-      dumpFile: String(R.defaultTo('./leetbot/dump.json', process.env.LEETBOT_DUMP_FILE)),
-      dumpCron: R.defaultTo('* * * * *', process.env.LEETBOT_DUMP_CRON),
-      timezone,
-      admin: process.env.LEETBOT_ADMIN
+  return {
+    examplebot: {
+      token: R.defaultTo('', process.env.EXAMPLEBOT_TOKEN),
+      username: R.defaultTo('', process.env.EXAMPLEBOT_USERNAME),
+      config: {}
+    },
+    leetbot: {
+      token: R.defaultTo('', process.env.LEETBOT_TOKEN),
+      username: R.defaultTo('', process.env.LEETBOT_USERNAME),
+      config: {
+        commit: process.env.COMMIT,
+        leetHours: parseHours(R.defaultTo(13, process.env.LEETBOT_HOURS), timezone),
+        leetMinutes: parseMinutes(R.defaultTo(37, process.env.LEETBOT_MINUTES), timezone),
+        dumpFile: String(R.defaultTo('./leetbot/dump.json', process.env.LEETBOT_DUMP_FILE)),
+        dumpCron: R.defaultTo('* * * * *', process.env.LEETBOT_DUMP_CRON),
+        timezone,
+        admin: process.env.LEETBOT_ADMIN
+      }
     }
   }
-})
+}
 
 export {
   loadConfig
