2b41eb212d963c3c64a110607608daf83a6a437a
Fritz-theCat <38458593+Fritz-theCat@users.noreply.github.com>
2019-03-12 14:00:03 +0100

    [FEATURE] add new insults in timing and asshole (#25)
    * new insults in timing and asshole
    
    * Update package.json

--- leetbot/i18n.js
+++ leetbot/i18n.js
@@ -10,7 +10,9 @@ i18next.init({
         'disable chat': 'Leeten ist vorbei. Tschüssi!',
         'callout': {
           'asshole': [
-            'DUUU DRECKIGERS STUK SCHEIẞE WARUM MACHST DU SWOWAS\nMACH DES JA NET NOCHMAL DO SCHMOK WAS DA LOS\nALLE AMBARSCH NACH HAUSE LEET ZEIT IS VORBEI WGEEN {{asshole, uppercase}}'
+            'DUUU DRECKIGERS STUK SCHEIẞE WARUM MACHST DU SWOWAS\nMACH DES JA NET NOCHMAL DO SCHMOK WAS DA LOS\nALLE AMBARSCH NACH HAUSE LEET ZEIT IS VORBEI WGEEN {{asshole, uppercase}}',
+            'OHHHH Mann {{asshole}}, Du hattes genau eine Aufgabe und hast nicht mal das hinbekommen, wie kann man sich als so ein Verlierer noch in der Öffentlichkeit sehen lassen? Wie schwer kann das denn sein?',
+            'Als die Intelligenz verteilt wurde war {{asshole}} wohl grade kacken, anders kann ich mir nicht erklären wie man so jämmerlich versagen kann.'
           ],
           'timing': [
             'digga hast du eine uhr? mach ma so sachen nicht',
@@ -19,7 +21,11 @@ i18next.init({
             'ob du ne uhr hast?',
             'peter lustig seine oma ist stolz auf dich. fischkopf',
             'ee du otto. is grad echt nich zeit für den spaß lol',
-            'wolltest wohl @MeisterRados pingen? hat mal wieder ewig gedauert, des is @akampf aber auch'
+            'wolltest wohl @MeisterRados pingen? hat mal wieder ewig gedauert, des is @akampf aber auch',
+            'Ich bin nicht wütend, ich bin nur enttäuscht.',
+            'Wer hat mich schon wieder ohne Grund geweckt?',
+            'Je öfter man 1337 schreibt desto witziger wirds... NICHT!',
+            'GuCkT mAl HeR lEuTe IcH bIn EiN dEpP uNd WiLl AuFmErKsAmKeIt.'
           ]
         },
         'report': {
