d33e01b836b11ee440dc01fc14002d70efbf5570
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2019-12-08 19:37:32 +0100

    fix: Parse config values and set correct defaults.

--- src/util/config.js
+++ src/util/config.js
@@ -1,23 +1,26 @@
 const path = require("path");
 
 const { flaschenpost } = require("flaschenpost");
+const parseStringsInObject = require("parse-strings-in-object");
 const rc = require("rc");
 
 const logger = flaschenpost.getLogger();
 
 const loadConfig = () => {
-  const config = rc("leetbot", {
-    token: "",
-    username: "YeldirsLeetBot",
-    bot: {
-      leetHour: 19,
-      leetMinute: 29,
-      timezone: "Europe/Berlin",
-      dumpFile: path.join(".", "leetbot", "dump.json"),
-      dumpCron: "* * * * *",
-      admin: undefined
-    }
-  });
+  const config = parseStringsInObject(
+    rc("leetbot", {
+      token: "",
+      username: "YeldirsLeetBot",
+      bot: {
+        leetHour: 13,
+        leetMinute: 37,
+        timezone: "Europe/Berlin",
+        dumpFile: path.join(".", "leetbot", "dump.json"),
+        dumpCron: "* * * * *",
+        admin: undefined
+      }
+    })
+  );
 
   logger.info("Configuration loaded.", {
     ...config,
